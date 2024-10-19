5689c9c49cb514b39d12c19c66d26097dd11dd91
Minger <149152892+CXL97@users.noreply.github.com>
2024-10-20 00:27:38 +0800

    Update TradeQueueManager.cs

--- SysBot.Pokemon/Queues/TradeQueueManager.cs
+++ SysBot.Pokemon/Queues/TradeQueueManager.cs
@@ -51,7 +51,7 @@ public class TradeQueueManager<T> where T : PKM, new()
 
         var random = Hub.Ledy.Pool.GetRandomPoke();
         var code = cfg.RandomCode ? Hub.Config.Trade.GetRandomTradeCode() : cfg.TradeCode;
-        var trainer = new PokeTradeTrainerInfo("Random Distribution");
+        var trainer = new PokeTradeTrainerInfo("随机派送");
         detail = new PokeTradeDetail<T>(random, trainer, PokeTradeHub<T>.LogNotifier, PokeTradeType.Random, code);
         return true;
     }
