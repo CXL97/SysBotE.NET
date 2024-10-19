8a4a4181d0423c97d251c54dbe457c053a5d6f52
Minger <149152892+CXL97@users.noreply.github.com>
2024-10-19 21:11:21 +0800

    Update 汉化

--- SysBot.Pokemon/Settings/DistributionSettings.cs
+++ SysBot.Pokemon/Settings/DistributionSettings.cs
@@ -8,29 +8,29 @@ public class DistributionSettings : ISynchronizationSetting
 {
     private const string Distribute = nameof(Distribute);
     private const string Synchronize = nameof(Synchronize);
-    public override string ToString() => "Distribution Trade Settings";
+    public override string ToString() => "派送机器人设置";
 
     // Distribute
 
-    [Category(Distribute), Description("When enabled, idle LinkTrade bots will randomly distribute PKM files from the DistributeFolder.")]
+    [Category(Distribute), Description("当启用时，空闲的连接交换机器人将从Distribute文件夹分发PKM文件。")]
     public bool DistributeWhileIdle { get; set; } = true;
 
-    [Category(Distribute), Description("When enabled, the DistributionFolder will yield randomly rather than in the same sequence.")]
+    [Category(Distribute), Description("当启用后，Distribution文件夹将随机产生，而不是按照相同的顺序产生。")]
     public bool Shuffled { get; set; }
 
-    [Category(Distribute), Description("When set to something other than None, the Random Trades will require this species in addition to the nickname match.")]
+    [Category(Distribute), Description("当设置为None以外的值时，随机交易除了匹配昵称外，还需要匹配这个种类的宝可梦。")]
     public Species LedySpecies { get; set; } = Species.None;
 
-    [Category(Distribute), Description("When set to true, Random Ledy nickname-swap trades will quit rather than trade a random entity from the pool.")]
+    [Category(Distribute), Description("当设置为True时，随机的Ledy nickname-swap交易将退出，而不是从交易文件夹中随机交易一个宝可梦。")]
     public bool LedyQuitIfNoMatch { get; set; }
 
-    [Category(Distribute), Description("Distribution Trade Link Code.")]
+    [Category(Distribute), Description("派送交换连接密码(0-99999999)")]
     public int TradeCode { get; set; } = 7196;
 
-    [Category(Distribute), Description("Distribution Trade Link Code uses the Min and Max range rather than the fixed trade code.")]
+    [Category(Distribute), Description("派送交换连接密码使用最小值和最大值范围，而不是固定的交换密码。")]
     public bool RandomCode { get; set; }
 
-    [Category(Distribute), Description("For BDSP, the distribution bot will go to a specific room and remain there until the bot is stopped.")]
+    [Category(Distribute), Description("对于[珍钻]，派送机器人将进入一个特定的房间并保持在那里，直到机器人停止。")]
     public bool RemainInUnionRoomBDSP { get; set; } = true;
 
     // Synchronize
