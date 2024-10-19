7d1fd4653675153f3baddf638c597f560fe24670
Minger <149152892+CXL97@users.noreply.github.com>
2024-10-19 21:08:16 +0800

    Update 汉化

--- SysBot.Pokemon/Settings/BaseConfig.cs
+++ SysBot.Pokemon/Settings/BaseConfig.cs
@@ -11,16 +11,16 @@ public abstract class BaseConfig
     protected const string Operation = nameof(Operation);
     private const string Debug = nameof(Debug);
 
-    [Category(FeatureToggle), Description("When enabled, the bot will press the B button occasionally when it is not processing anything (to avoid sleep).")]
+    [Category(FeatureToggle), Description("当启用时，机器人会在不处理任何东西时偶尔按下B按钮(以避免睡眠)。")]
     public bool AntiIdle { get; set; }
 
-    [Category(FeatureToggle), Description("Enables text logs. Restart to apply changes.")]
+    [Category(FeatureToggle), Description("启用文本日志记录，在应用更改后重启。")]
     public bool LoggingEnabled { get; set; } = true;
 
-    [Category(FeatureToggle), Description("Maximum number of old text log files to retain. Set this to <= 0 to disable log cleanup. Restart to apply changes.")]
+    [Category(FeatureToggle), Description("保留的旧文本日志文件的最大数量。将其设置为<=0以禁用日志清理。重新启动以应用更改")]
     public int MaxArchiveFiles { get; set; } = 14;
 
-    [Category(Debug), Description("Skips creating bots when the program is started; helpful for testing integrations.")]
+    [Category(Debug), Description("在程序启动时跳过创建机器人；有助于测试集成。")]
     public bool SkipConsoleBotCreation { get; set; }
 
     [Category(Operation)]
